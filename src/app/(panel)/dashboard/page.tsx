e123678aa4fb7936bd13fc13cd2904574571d380
CaioMiranda12 <caioclm04@gmail.com>
2025-07-15 19:20:47 -0300

    feat: create function getUserData on Profile page

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -4,12 +4,9 @@ import { redirect } from "next/navigation"
 export default async function Dashboard() {
   const session = await getSession()
 
-
   if (!session) {
     redirect('/')
   }
-
-
 
   return (
     <div>
--- src/app/(panel)/dashboard/profile/page.tsx
+++ src/app/(panel)/dashboard/profile/page.tsx
@@ -1,4 +1,21 @@
-export default function Profile() {
+import getSession from '@/lib/getSession'
+import { redirect } from 'next/navigation'
+import getUserData from './_data-acess/get-info-user'
+
+
+export default async function Profile() {
+  const session = await getSession()
+
+  if (!session) {
+    redirect('/')
+  }
+
+  const user = await getUserData({ userId: session.user?.id })
+
+  if (!user) {
+    redirect('/')
+  }
+
   return (
     <section>
       Perfil
